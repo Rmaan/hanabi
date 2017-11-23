657ee92f6eaaf37c228e8a5c140151902cbe132d
Arman Ordookhani <arman.ordookhani@gmail.com>
2017-11-23 13:15:45 +0330

    Rewrote most of frontend using a simple VDOM tool (maquette)

--- static/js/client.js
+++ static/js/client.js
@@ -15,98 +15,159 @@
 })();
 
 window.addEventListener("load", function() {
-    document.body.innerHTML = `
-<div id="top-bar">
-	<div id="debug"></div>
-	<button id='btn-dc'>DC</button>
-</div>
-<div id="canvas">
-	<div id="status"></div>
-    <div class="desk"></div>
-    <div class="hanabis"></div>
-    <div class="others-command-pallet hide">
-        <button class="cmd-hint-color">Hint color (<span></span>)</button>
-        <button class="cmd-hint-number">Hint number (<span></span>)</button>
-    </div>
-    <div class="player-0 players">
-        <div class="cards"></div>
-        <div class="name"></div>
-        <div class="self-command-pallet hide">
-            <button class="cmd-play">Play</button>
-            <button class="cmd-discard">Discard</button>
-        </div>
-    </div>
-    <div class="player-1 players"><div class="cards"></div><div class="name"></div></div>
-    <div class="player-2 players"><div class="cards"></div><div class="name"></div></div>
-    <div class="player-3 players"><div class="cards"></div><div class="name"></div></div>
-    <div class="player-4 players"><div class="cards"></div><div class="name"></div></div>
-    <div id="msg-box"><div class="msg-log"></div><div class="send"><form><input type="text"><button type="submit">Send</button></form></div></div>
-</div>
-`
+    var h = maquette.h;
+    var projector = maquette.createProjector();
 
-    var $canvas = document.getElementById('canvas');
-    var $status = document.getElementById('status');
-    var $debug = document.getElementById('debug');
-    var $hanabis = document.querySelector('.hanabis');
-    var $selfCmdPallet = document.querySelector('.self-command-pallet');
-    var $othersCmdPallet = document.querySelector('.others-command-pallet');
-    var $msgBox = document.getElementById('msg-box');
-    var $msgLog = $msgBox.querySelector('.msg-log');
+    var status = 'Hello!';
+    var debug = 'debug bar';
+    // World networked from server
+    var w = {
+        SuccessfulPlayedCount: [],
+        Players: [],
+        DeskObjects: [],
+    }
+    var allMsgs = []  // All chat logs
     var hoveredSelfCardIndex = null
-    var lastMoveLocationX, lastMoveLocationY
     var hoveredOthersCard = {
         active: false,
         playerId: null,
         card: null,
-    }
-    var $players = _.range(5).map(idx => document.querySelector('.player-' + idx))
-    var $playerCards = $players.map(el => el.querySelector('.cards'))
-    var $selfCards = $playerCards[0]
-    var $othersCards = $playerCards.slice(1)
-
-    // Browsers doesn't support passing data to ondragover (for a reason I don't know) this is a simple workaround
-    // assuming there is only one drag (no multi touch).
-    var dragging = {
-        objectBeingDragged: null,
-        gripOffsetX: 0,
-        gripOffsetY: 0,
+        $card: null,
     }
 
-    $canvas.ondrop = ev => {
-        // ev.preventDefault()
-        console.log('drop', ev,  ev.dataTransfer.getData("text"))
+    // Do first render
+    projector.append(document.body, render);
+
+    var $canvas = document.getElementById('canvas');
+    var $msgBox = document.getElementById('msg-box');
+    var $msgLog = $msgBox.querySelector('.msg-log');
+
+    function renderHanabis() {
+        return h('div.hanabis',
+            w.SuccessfulPlayedCount.map(
+                (count, idx) => h('div.obj_desk_card', {key: idx, styles: {'background-image': `url("/static/img/spirits/${idx + 1}${count}.png")`}})
+            )
+        )
     }
 
+    function renderMsgBox() {
+        return h('div#msg-box', [
+            h('div.msg-log', allMsgs.map((log, idx) =>
+                h('div.line', {key: idx, classes: {"system-msg": !log.IsChat}}, [
+                    h('span.player-name', [w.Players[log.PlayerId].Name]),
+                    ': ',
+                    h('span.text', [log.Text])
+                ])
+            )),
+            h('div.send', [
+                h('form', {
+                    onsubmit: onSubmitChat,
+                }, [
+                    h('input', {type: 'text'}),
+                    h('button', {type: 'submit'}, ['Send']),
+                ])
+            ])
+        ])
+    }
 
-    document.getElementById('btn-dc').onclick = () => ws.close()
+    function renderPlayer(playerId) {
+        var player = w.Players[playerId]
+        if (!player)
+            return null
+
+        var isSelf = playerId == 0
+
+        var selfPallet = isSelf ? h('div.self-command-pallet.hide', {
+            classes: {hide: hoveredSelfCardIndex == null}
+        }, [
+            h('button.cmd-play', {onclick: onCmdPlay}, ['Play']),
+            h('button.cmd-discard', {onclick: onCmdDiscard}, ['Discard']),
+        ]) : null
+
+        return h(`div.players.player-${playerId}`, [
+            h('div.cards', player.Cards.map((card, idx) =>
+                h('div.obj_player_card', {
+                    styles: {backgroundImage: `url("/static/img/spirits/${card.Color}${card.Number}.png")`},
+                    classes: {hover: isSelf && idx == hoveredSelfCardIndex},
+                    key: card.id,
+                    onclick: isSelf ? (e => hoveredSelfCardIndex = idx) : (e => setOthersCardHoverState(true, playerId, card, e.target))
+                })
+            )),
+            h('div.name', [player.Name]),
+            selfPallet
+        ])
+    }
+
+    function renderOthersCmdPallet() {
+        if (!hoveredOthersCard.active)
+            return null
+
+        // Want to place command pallet near the card clicked. We extract some coordinates using DOM directly.
+        var playerCardRect = document.querySelector(`.player-${hoveredOthersCard.playerId} .cards`).getClientRects()[0]
+        var canvasRect = $canvas.getClientRects()[0]
+        var cardRect = hoveredOthersCard.$card.getClientRects()[0]
+
+        var styles = {
+            top: cardRect.y - canvasRect.y + 'px',
+            right: '',
+            left: '',
+        }
+
+        if (hoveredOthersCard.playerId == 1 || hoveredOthersCard.playerId == 2) {
+            styles.left = playerCardRect.width + 'px'
+        } else if (hoveredOthersCard.playerId == 3 || hoveredOthersCard.playerId == 4) {
+            styles.right = playerCardRect.width + 'px'
+        }
+
+        return h('div.others-command-pallet', {
+            styles: styles,
+        }, [
+            h('button.cmd-hint-color', {onclick: onCmdHintColor}, [`Hint color (${hoveredOthersCard.card.Color})`]),
+            h('button.cmd-hint-number', {onclick: onCmdHintNumber}, [`Hint number (${hoveredOthersCard.card.Number})`]),
+        ])
+    }
+
+    function renderDesk() {
+        return h('div.desk', w.DeskObjects.map(obj =>
+            h('div.obj_block', {
+                styles: {
+                    top: obj.Y + 'px',
+                    left: obj.X + 'px',
+                    width: obj.Width.toString(),
+                    height: obj.Height.toString(),
+                },
+            })
+        ))
+
+    }
+
+    function render() {
+        return h('div.render', [
+            h('div.top-bar', [
+                h('div#debug', [debug]),
+                h('button#btn-dc', {onclick: e => ws.close()}, ['DC'])
+            ]),
+            h('div#canvas', [
+                h('div#status', [status]),
+                renderDesk(),
+                renderHanabis(),
+                renderOthersCmdPallet(),
+                renderPlayer(0),
+                renderPlayer(1),
+                renderPlayer(2),
+                renderPlayer(3),
+                renderPlayer(4),
+                renderMsgBox(),
+            ]),
+        ]);
+    }
 
     function sendCommand(type, params) {
         var data = JSON.stringify({type: type, params: params})
-        $debug.innerText = +new Date() + " " + data;
+        debug = +new Date() + " " + data;
         ws.send(data)
+        projector.scheduleRender()
     }
-
-    var sendMoveCommand = _.throttle(ev => {
-        // on drag over will be called multiple times event if you don't move your cursor.
-        if (ev.clientX == lastMoveLocationX && ev.clientY == lastMoveLocationY)
-            return
-        lastMoveLocationX = ev.clientX
-        lastMoveLocationY = ev.clientY
-
-        var commandParams = {
-            'TargetId': dragging.objectBeingDragged,
-            'X': ev.clientX - $canvas.getClientRects()[0].x - dragging.gripOffsetX,
-            'Y': ev.clientY - $canvas.getClientRects()[0].y - dragging.gripOffsetY,
-        }
-        sendCommand('move', commandParams)
-    }, 50, {leading: false})
-
-    $canvas.ondragover = ev => {
-        ev.preventDefault()
-        ev.dataTransfer.dropEffect = "move"
-        sendMoveCommand(ev)
-    }
-
 
     function hintPlayer(playerId, isColor, value) {
         sendCommand('hint', {
@@ -128,104 +189,49 @@ window.addEventListener("load", function() {
         })
     }
 
-    function getChildNumber(node) {
-        return Array.prototype.indexOf.call(node.parentNode.childNodes, node);
-    }
-
-    function hoverUnhoverOthersCard(active, playerId, $card) {
-        // If active is false will unhover
-
-        if (hoveredOthersCard.active == active && (!active || hoveredOthersCard.playerId == playerId && hoveredOthersCard.card == $card.extra.gameObj))
-            return
-
-        if (hoveredOthersCard.active) {
-
-        }
-
+    function setOthersCardHoverState(active, playerId, card, $card) {
         hoveredOthersCard.active = active
-
-        $othersCmdPallet.classList.toggle('hide', !active)
-
-        if (hoveredOthersCard.active) {
+        if (active) {
             hoveredOthersCard.playerId = playerId
-            var card = hoveredOthersCard.card = $card.extra.gameObj
-            var playerCardRect = $playerCards[playerId].getClientRects()[0];
-            var canvasRect = $canvas.getClientRects()[0];
-            var cardRect = $card.getClientRects()[0]
-
-            if (playerId == 1 || playerId == 2) {
-                $othersCmdPallet.style.left = playerCardRect.width + 'px'
-                $othersCmdPallet.style.right = ''
-                $othersCmdPallet.style.top = cardRect.y - canvasRect.y + 'px'
-            }
-            if (playerId == 3 || playerId == 4) {
-                $othersCmdPallet.style.left = ''
-                $othersCmdPallet.style.right = playerCardRect.width + 'px'
-                $othersCmdPallet.style.top = cardRect.y - canvasRect.y + 'px'
-            }
-
-            $othersCmdPallet.querySelector('button:nth-child(1) span').textContent = card.Color
-            $othersCmdPallet.querySelector('button:nth-child(2) span').textContent = card.Number
+            hoveredOthersCard.card = card
+            hoveredOthersCard.$card = $card
+        } else {
+            hoveredOthersCard.playerId = null
+            hoveredOthersCard.card = null
+            hoveredOthersCard.$card = null
         }
     }
 
-    $othersCards.forEach(($cards, idx) => $cards.onclick = e => {
-        if (e.target.classList.contains('obj_player_card')) {
-            hoverUnhoverOthersCard(true, idx + 1, e.target)
-        }
-    })
-
-    document.body.addEventListener('click', e => {
-        if (!e.target.classList.contains('obj_player_card')) {
-            hoverUnhoverOthersCard(false)
-        }
-    })
-
-    document.querySelector('.cmd-hint-color').onclick = e => hintPlayer(hoveredOthersCard.playerId, true, hoveredOthersCard.card.Color)
-    document.querySelector('.cmd-hint-number').onclick = e => hintPlayer(hoveredOthersCard.playerId, false, hoveredOthersCard.card.Number)
-
-    function hoverUnhoverSelfCard(index) {
-        // Pass index to hover a specific card.
-        // Pass null to unhover hovered card.
-        // Can be called multiple times with same parameter without making a mess.
-        if (hoveredSelfCardIndex === index)
-            return
-
-        if (hoveredSelfCardIndex !== null) {
-            $selfCards.childNodes[hoveredSelfCardIndex].classList.remove('hover')
-        }
-
-        hoveredSelfCardIndex = index
-        $selfCmdPallet.classList.toggle('hide', hoveredSelfCardIndex === null)
-
-        if (hoveredSelfCardIndex !== null) {
-            $selfCards.childNodes[hoveredSelfCardIndex].classList.add('hover')
-        }
+    function onCmdHintColor(e) {
+        hintPlayer(hoveredOthersCard.playerId, true, hoveredOthersCard.card.Color)
     }
 
-    $selfCards.onclick = e => {
-        if (e.target.classList.contains('obj_player_card')) {
-            hoverUnhoverSelfCard(getChildNumber(e.target))
-        }
+    function onCmdHintNumber(e) {
+        hintPlayer(hoveredOthersCard.playerId, false, hoveredOthersCard.card.Number)
     }
 
-    document.querySelector('.cmd-play').onclick = e => {
+    function onCmdPlay(e) {
         playCard(hoveredSelfCardIndex)
-        hoverUnhoverSelfCard(null)
+        hoveredSelfCardIndex = null
+        projector.scheduleRender()
     }
 
-    document.querySelector('.cmd-discard').onclick = e => {
+    function onCmdDiscard(e) {
         discardCard(hoveredSelfCardIndex)
-        hoverUnhoverSelfCard(null)
+        hoveredSelfCardIndex = null
+        projector.scheduleRender()
     }
 
+    // Hide pallets when clicked on somewhere else
     document.body.addEventListener('click', e => {
         if (!e.target.classList.contains('obj_player_card')) {
-            hoverUnhoverSelfCard(null)
+            hoveredSelfCardIndex = null  // unhover self
+            setOthersCardHoverState(false)  // unhover others
+            projector.scheduleRender()
         }
     })
 
-    $msgBox.querySelector('form').onsubmit = e => {
+    function onSubmitChat(e) {
         e.preventDefault()
         var $inp = e.target.querySelector('input')
         sendCommand('chat', {
@@ -234,129 +240,40 @@ window.addEventListener("load", function() {
         $inp.value = ''
     }
 
-    function getObjectDiv(obj, $parent) {
-        var domId = 'game-id-' + obj.Id
-        var $o = document.getElementById(domId)
-        if (!$o) {
-            $o = document.createElement('div')
-            $o.id = domId
-            $o.classList.add('obj_' + obj.Class, 'game-obj')
-            $o.extra = {
-                gameObj: obj,
-                gameClass: obj.Class,
-            }
+    function setNewWorld(world) {
+        w = world;
+        window.w = w;
+        status = `tick=${world.TickNumber} hint_token=${world.HintTokenCount} mistake_token=${world.MistakeTokenCount} deck_cards=${world.RemainingDeckCount}`;
+        allMsgs = allMsgs.concat(world.NewLogs)
 
-            if (obj.Class == 'desk_item') {
-                $o.draggable = true
-                $o.ondragstart = ev => {
-                    dragging = {
-                        objectBeingDragged: obj.Id,
-                        gripOffsetX: ev.clientX - ev.target.getClientRects()[0].x,
-                        gripOffsetY: ev.clientY - ev.target.getClientRects()[0].y,
-                    }
-                    console.log('drag start', dragging)
-                }
-            }
-
-            $parent.appendChild($o)
-        }
-        return $o
-    }
-
-    function drawWorld(world) {
-        console.log('drawing', world)
-
-        $status.textContent = `tick=${world.TickNumber} hint_token=${world.HintTokenCount} mistake_token=${world.MistakeTokenCount} deck_cards=${world.RemainingDeckCount}`;
-
-        // TODO avoid creating divs each tick.
-        $hanabis.innerHTML = ''
-        world.SuccessfulPlayedCount.forEach((count, idx) => {
-            var $hanabi = document.createElement('div')
-            $hanabi.className = 'obj_desk_card'
-            $hanabi.style.backgroundImage = `url("/static/img/spirits/${idx + 1}${count}.png")`
-            $hanabis.appendChild($hanabi)
-        })
-
-        var allObjectsClass = {}
-        document.querySelectorAll('.game-obj').forEach(el => {
-            allObjectsClass[el.id] = el.extra.gameClass
-        })
-
-        // TODO support deleting div of removed objects
-        // TODO remove Desk from client/server
-        world.DeskObjects.forEach(obj => {
-            if (obj.SpiritId) {
-                obj.Class = 'desk_item'
-            } else {
-                obj.Class = 'block'
-            }
-            var $o = getObjectDiv(obj, $canvas)
-
-            $o.style.top = obj.Y + 'px'
-            $o.style.left = obj.X + 'px'
-
-            if (obj.SpiritId) {
-                $o.style.backgroundImage = `url("/static/img/spirits/${obj.SpiritId}.png")`
-            }
-        })
-
-        world.Players.forEach((p, playerIndex) => {
-            $players[playerIndex].querySelector('.name').textContent = p.Name
-
-            p.Cards.forEach(obj => {
-                obj.Class = 'player_card'
-                var $o = getObjectDiv(obj, $playerCards[playerIndex])
-
-                $o.style.backgroundImage = `url("/static/img/spirits/${obj.Color}${obj.Number}.png")`
-
-                delete allObjectsClass[$o.id] // Remove them from dict so they remain in DOM
-            })
-        })
-
-        // Remove all non-networked cards from DOM
-        Object.keys(allObjectsClass).forEach(id => {
-            if (allObjectsClass[id] == 'player_card') {
-                document.getElementById(id).remove()
-            }
-        })
-
-        world.NewLogs.forEach(log => {
-            var $el = htmlToDom(`<div class="line"><span class="player-name">${world.Players[log.PlayerId].Name}</span>: <span class="text">${log.Text}</span></div>`)
-            $msgLog.appendChild($el)
-            $el.classList.toggle("system-msg", !log.IsChat)
-        })
-
-        // Auto scroll down
+        // Auto scroll down msg log
         if (world.NewLogs.length)
             $msgLog.scrollTop = $msgLog.scrollHeight
-    }
 
-    function htmlToDom(htmlString) {
-        var wrapper= document.createElement('div');
-        wrapper.innerHTML= htmlString;
-        return wrapper.firstChild;
+        projector.scheduleRender()
     }
 
     var ws = new WebSocket(window.args.ws_url);
     ws.binaryType = "arraybuffer"
 
     ws.onopen = function(e) {
-        $status.textContent = "WS open"
+        status = "WS open"
+        projector.scheduleRender()
     }
 
     ws.onclose = function(e) {
-        $status.textContent = "WS closed :("
+        status = "WS closed :("
+        projector.scheduleRender()
     }
 
     ws.onmessage = function(e) {
         var buffer = new Uint8Array(e.data)
-        drawWorld(msgpack.decode(buffer))
+        setNewWorld(msgpack.decode(buffer))
     }
 
     ws.onerror = function(e) {
         console.log("WS err: " + e.data);
     }
 
-    window.ws = ws
     window.cmd = sendCommand
 });
